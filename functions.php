90ed9944eedaf1574421cfb4e154ea9382ad2619
heshan8 <heshan988@gmail.com>
2025-03-28 23:25:05 +1000

    Move theme files to root

--- functions.php
+++ functions.php
@@ -26,7 +26,7 @@ function _s_setup() {
 		* If you're building a theme based on _s, use a find and replace
 		* to change '_s' to the name of your theme in all the template files.
 		*/
-	load_theme_textdomain( '_s', get_template_directory() . '/languages' );
+	load_theme_textdomain( '_s', get_template_directory() . '/languages');
 
 	// Add default posts and comments RSS feed links to head.
 	add_theme_support( 'automatic-feed-links' );
