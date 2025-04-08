b3a2313f42e70bf9e371c648ef86933ca2eecdc9
heshan8 <heshan988@gmail.com>
2025-04-08 13:18:28 +1000

    Revert "Add full-width image support to block editor"
    This reverts commit bda0dc00df020c92ea1e0f94416f37337e252f18.

--- functions.php
+++ functions.php
@@ -266,5 +266,4 @@ require get_template_directory() . '/inc/customizer.php';
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-add_theme_support( 'align-wide' );
 
